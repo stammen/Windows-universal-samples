88e81f34dffde0e98fa21e387710c24c956a6fca
Dale Stammen <dalestam@microsoft.com>
2016-10-04 17:19:07 -0700

    added scenario 7

--- Samples/SimpleInk/cpp/Scenario7.xaml.h
+++ Samples/SimpleInk/cpp/Scenario7.xaml.h
@@ -24,11 +24,15 @@ namespace SDKTemplate
     {
     public:
         Scenario7();
+
+	protected:
+		virtual void OnNavigatedTo(Windows::UI::Xaml::Navigation::NavigationEventArgs^ e) override;
+
     private:
-        MainPage^ rootPage;
-        void Button_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
-        void Button_Click_1(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
-        void Button_Click_2(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
-        void Button_Click_3(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void InkToolbar_IsRulerButtonCheckedChanged(Windows::UI::Xaml::Controls::InkToolbar^ sender, Platform::Object^ args);
+		void OnBringIntoView(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		
+		MainPage^ rootPage;
+		Windows::UI::Input::Inking::InkPresenterRuler^ ruler;
     };
 }
